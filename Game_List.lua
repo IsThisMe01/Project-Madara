d7a2a40cadba48ff1b9743d38f950523a0558316
IsThisMe01 <150212080+IsThisMe01@users.noreply.github.com>
2025-09-03 19:07:33 -0700

    Update Game_List.lua

--- Game_List.lua
+++ Game_List.lua
@@ -1,5 +1,5 @@
 local games = {
-    [121864768012064] = "" -- Legends of Speed
+    [121864768012064] = "https://raw.githubusercontent.com/IsThisMe01/Project-Madara/refs/heads/main/fishit.lua",
 }
 
 return games
