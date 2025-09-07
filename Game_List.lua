f1fefcb83b3e2b6bef65c5d6febddb2e686c111f
IsThisMe01 <150212080+IsThisMe01@users.noreply.github.com>
2025-09-07 12:11:36 -0700

    Update Game_List.lua

--- Game_List.lua
+++ Game_List.lua
@@ -1,5 +1,8 @@
 local games = {
     [121864768012064] = "https://raw.githubusercontent.com/IsThisMe01/Project-Madara/refs/heads/main/fishit.lua",
+    [99567941238278] = "https://raw.githubusercontent.com/IsThisMe01/Project-Madara/refs/heads/main/inkGame.lua", -- Main Lobby
+    [125009265613167] = "https://raw.githubusercontent.com/IsThisMe01/Project-Madara/refs/heads/main/inkGame.lua", -- Game mode
+
 }
 
 return games
