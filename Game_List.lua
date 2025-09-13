0df15d3cd286854f2c36e62fce113bb1cf64b42c
IsThisMe01 <150212080+IsThisMe01@users.noreply.github.com>
2025-09-12 19:43:00 -0700

    Update Game_List.lua

--- Game_List.lua
+++ Game_List.lua
@@ -4,6 +4,7 @@ local games = {
     [125009265613167] = "https://raw.githubusercontent.com/IsThisMe01/Project-Madara/refs/heads/main/inkGame.lua", -- Game mode
     [126884695634066] = "https://raw.githubusercontent.com/IsThisMe01/Project-Madara/refs/heads/main/growAGarden.lua",
     [3956818381] = "https://raw.githubusercontent.com/IsThisMe01/Project-Madara/refs/heads/main/ninjaLegends.lua",
+    [3101667897] = "https://raw.githubusercontent.com/IsThisMe01/Project-Madara/refs/heads/main/LegendsOfSpeed.lua",
 
 }
 
